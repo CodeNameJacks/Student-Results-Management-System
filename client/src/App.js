ee860bc011a2c72d74b7c1190f137bf0e73b3db2
CodeNameJacks <jmedley60@hotmail.com>
2024-02-17 17:07:53 -0500

    Added Home and New Student routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,36 @@
 import React from "react";
-import {useEffect, useState} from "react";
+//import {useEffect, useState} from "react";
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 //import {} from 'react-redux';
-//import axios from "axios";
+//import 'bootstrap/dist/css/bootstrap.min.css';
+//import "bootstrap/dist/css/bootstrap.min.css";
+import axios from "axios";
 import logo from './logo.svg';
 import './App.css';
 
+/* Add/Import Routes */
+import Home from './element/Home'
+import NewStudent from './element/NewStudent' //do this for the other components
+
+
 function App() {
-  const [ data, setData] = useState("");
+  //const [ data, setData] = useState("");  KEEP THIS FOR TESTING INITIAL BASE SETUP
   
-  useEffect(() => {
+  /*useEffect(() => {
     fetch("/api")
     .then((res) => res.json())
     .then((data) => setData(data.message));
-  },[]);
+  },[]);*/
 
-  
-  
+  //add other routes below - see section 6:57
   return (
-    <div className="App">
-      <h1>{data}</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>} /> 
+        <Route path='/NewStudent' element={<NewStudent/>}/> 
+        
+      </Routes>
+    </BrowserRouter>
   );
 }
 
