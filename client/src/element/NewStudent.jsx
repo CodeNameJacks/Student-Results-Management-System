56b47bf3f88e52f36ec2c1e85b1964cd9ee41477
CodeNameJacks <jmedley60@hotmail.com>
2024-02-17 16:47:47 -0500

    Created NewStudents component and api

---
+++ client/src/element/NewStudent.jsx
@@ -0,0 +1,67 @@
+import React, {useState} from 'react'
+import axios from 'axios'
+import {Link, useNavigate} from 'react-router-dom'
+//import 'bootstrap/dist/css/boorstrap.min.css'
+
+function NewStudent() {
+    const [values, setValues] = useState({
+        firstName: "",
+        lastName: "",
+        sin:"",
+        dob: "",
+        email: ""
+
+    })
+
+    function handleSubmit(e) {
+        e.preventDefault() //prevent default orm sumission behaviour
+        
+        axios.post('/add_student', values)
+        .then((res) => {
+            var mes = JSON.stringify(res.data.message);
+            alert(mes);
+            console.log(res);
+        })
+        .catch((err) => {
+            alert("The student already exist. 2");
+            console.log(err);
+        });
+    };
+
+
+    return (
+        <div className = 'container vh-100 vw-100 bg-primary'> 
+        <div className='row'>
+            <div className='d-flex justify-content-end'>
+                <Link to='/' className='btn btn-success'>Home</Link>
+            </div>
+            <form onSubmit = {handleSubmit}> 
+                <div className="form-group my-3">
+                    <label>Firstname: </label>
+                    <input type='text' name='firstName' required onChange={(e) => setValues({...values, firstName: e.target.value})}></input>
+                </div>
+                <div className="form-group my-3">
+                    <label>Lastname: </label>
+                    <input type='text' name='lastName' required onChange={(e) => setValues({...values, lastName: e.target.value})}></input>
+                </div>
+                <div className="form-group my-3">
+                    <label>Date of Birth: </label>
+                    <input type='text' name='dob' required onChange={(e) => setValues({...values, dob: e.target.value})}></input>
+                </div>
+                <div className="form-group my-3">
+                    <label>SIN: </label>
+                    <input type='text' name='sin' required onChange={(e) => setValues({...values, sin: e.target.value})}></input>
+                </div>
+                <div className="form-group my-3">
+                    <label>Email Address: </label>
+                    <input type='text' name='email' required onChange={(e) => setValues({...values, email: e.target.value})}></input>
+                </div>
+                <div className='form-group my-3'>
+                    <button type='submit' className='btn btn-success'>Save</button>
+                </div>
+            </form> 
+        </div>  
+        </div> 
+    )
+} export default NewStudent
+
--- server/index.js
+++ server/index.js
@@ -8,17 +8,85 @@ const PORT = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(express.json());
-app.unsubscribe(express.static(path.join(_dirname,"public")));
+app.use(express.static(path.join(__dirname, "public")));
 
 //establish connection to databsae
 const db = mysql.createConnection({
-  host: 'studentmgmt-db-student-mgmt-db.a.aivencloud.com'
-  user: 'avnadmin'
-  password: 'AVNS_Ul1fLFCrdz78zuqFNbx'
-  port: '12074'
+  host: 'studentmgmt-db-student-mgmt-db.a.aivencloud.com',
+  user: 'avnadmin',
+  password: 'AVNS_Ul1fLFCrdz78zuqFNbx',
+  port: '14074'
 });
 
-//create api endpoint - test is api works
+
+app.post('/add_student', (req, res) => {
+  count = 0;
+  const sqlFind = "COUNT (*) FROM stumgmtdb.Students where sin = ? ";
+  const sql = "INSERT INTO stumgmtdb.Students (`firstName`, `lastName`, `dob`, `email`, `sin`) VALUES (?,?,?,?,?)"; //inser dta into to table and binde 
+  const sinVal = [req.body.sin];
+  const values = [
+    req.body.firstName,
+    req.body.lastName,
+    req.body.dob,
+    req.body.email,
+    req.body.sin
+  ]
+ 
+  
+  db.query(sql, values, (err,result)=> {
+    if(err){
+      return res.json({message: "Could not add student or student already exists " + err});
+    }else {
+      return res.json({message: "Student added successfully"});
+    }
+  });
+   
+});
+
+
+app.post('update_student', (req, res) => {
+  sql = "UPDATE INTO students ('name', 'lastname', 'dob', 'email') VALUES (?)"; //inser dta into to table and binde 
+  const values = [
+    req.body.firstName,
+    req.body.lastName,
+    req.body.dob,
+    req.body.email
+  ]
+  db.query(sql, values, (err,result)=> {
+    if(err) { 
+      return res.json({message: "Could not update student"});
+    }else {
+      return res.json({success: "Student record successfully updated"});
+    }
+  })
+});
+
+app.get('/get_students', (req, res) => {
+  const sqlText = "SELECT * FROM students";
+  db.query(sqltext, (err,result) => {
+    if(err){
+      res.json({mesage: "Error retrieving students. Please try again."});
+    }else{
+      return res.json(result);
+    }
+  })
+});
+
+
+app.get('/get_students_byID/:id', (req, res) => {
+  const id = req.params.id;
+  const sqlText = "SELECT * FROM students where `id` = ?";
+  db.query(sqltext,[id], (err,result) => {
+    if(err){
+      res.json({mesage: "Error retrieiving informtion for that student. Please try again."});
+    }else{
+      return res.json(result);
+    }
+  })
+});
+
+
+//create api endpoint - Use this to test is api works
 app.get("/api", (req, res) => {
     res.json({ message: "Hello from server!" });
   });
