77282c7ccbc9a3bfa9bafb19a18c0f4b580feac1
CodeNameJacks <jmedley60@hotmail.com>
2024-02-28 19:10:56 -0500

    Styled NewStudent page

--- client/src/element/NewStudent.jsx
+++ client/src/element/NewStudent.jsx
@@ -2,6 +2,7 @@ import React, {useState} from 'react'
 import axios from 'axios'
 import {Link, useNavigate} from 'react-router-dom'
 //import 'bootstrap/dist/css/boorstrap.min.css'
+require('../assets/css/newStudent.css');
 
 function NewStudent() {
     const [values, setValues] = useState({
@@ -32,36 +33,36 @@ function NewStudent() {
 
     return (
         <div className = 'container vh-100 vw-100 bg-primary'> 
+       
         <div className='row'>
-            <div className='d-flex justify-content-end'>
-                <Link to='/' className='btn btn-success'>Home</Link>
-            </div>
             <form onSubmit = {handleSubmit}> 
+            <h1> Enter information for new student:</h1><br></br>
                 <div className="form-group my-3">
-                    <label>Firstname: </label>
+                    <label>Firstname: </label><br></br>
                     <input type='text' name='firstName' required onChange={(e) => setValues({...values, firstName: e.target.value})}></input>
                 </div>
                 <div className="form-group my-3">
-                    <label>Lastname: </label>
+                    <label>Lastname: </label><br></br>
                     <input type='text' name='lastName' required onChange={(e) => setValues({...values, lastName: e.target.value})}></input>
                 </div>
                 <div className="form-group my-3">
-                    <label>Date of Birth: </label>
+                    <label>Date of Birth: </label><br></br>
                     <input type='text' name='dob' required onChange={(e) => setValues({...values, dob: e.target.value})}></input>
                 </div>
                 <div className="form-group my-3">
-                    <label>SIN: </label>
+                    <label>SIN: </label><br></br>
                     <input type='text' name='sin' required onChange={(e) => setValues({...values, sin: e.target.value})}></input>
                 </div>
                 <div className="form-group my-3">
-                    <label>Email Address: </label>
+                    <label>Email Address: </label><br></br>
                     <input type='text' name='email' required onChange={(e) => setValues({...values, email: e.target.value})}></input>
                 </div>
                 <div className="form-group my-3">
-                    <label>Program: </label>
+                    <label>Program: </label><br></br>
                     <input type='text' name='email' required onChange={(e) => setValues({...values, program: e.target.value})}></input>
+                    <br></br><br></br>
                 </div>
-                <div className='form-group my-3'>
+                <div id="btn" className='form-group my-3'>
                     <button type='submit' className='btn btn-success'>Save</button>
                 </div>
             </form> 
